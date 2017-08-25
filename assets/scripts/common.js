9d080cd7d831a4d9309b856449f730c3cac4e965
galina-prokofeva <g.k.prokofeva@gmail.com>
2017-08-25 20:52:41 +0300

    square blocks

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,4 +1,9 @@
-
+if(document.getElementsByClassName('catalogue__item')){
+    $('.catalogue__item-image').each(function(){
+        $(this).children('a').css('background-image', 'url(' + $(this).find('img').attr('src') + ')' );
+        $(this).find('img').css('opacity', '0' );
+    });
+}
 
 
 
